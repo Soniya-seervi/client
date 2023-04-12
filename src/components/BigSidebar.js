efbfe3f7ebecfc2d8113acbd700cc3dfa0c88fc1
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-12 13:22:41 +0530

    Big Sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,9 +1,22 @@
 import Wrapper from '../assets/wrappers/BigSidebar'
+import NavLinks from './NavLinks'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 
 const BigSidebar = () => {
+
+  const {showSidebar} = useAppContext()
+
   return (
     <Wrapper>
-        <h1>BigSidebar</h1>
+        <div className={showSidebar ? 'sidebar-container': 'sidebar-container show-sidebar'}>
+          <div className="content">
+            <header>
+              <Logo/>
+            </header>
+            <NavLinks />
+          </div>
+        </div>
     </Wrapper>
   )
 }
