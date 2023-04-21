f08dda438ddb8cd14f502dde850cd04cb371e1e2
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-21 15:06:31 +0530

    Stats- front end

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -22,7 +22,9 @@ import {
     DELETE_JOB_BEGIN,
     EDIT_JOB_BEGIN,
     EDIT_JOB_SUCCESS,
-    EDIT_JOB_ERROR
+    EDIT_JOB_ERROR,
+    SHOW_STATS_BEGIN,
+    SHOW_STATS_SUCCESS
     } 
     from './actions'
 import axios from 'axios'
@@ -54,7 +56,9 @@ const initialState = {
     jobs: [],
     totalJobs: 0,
     numOfPages: 1,
-    page: 1
+    page: 1,
+    stats: {},
+    monthlyApplications: []
 
 }
 
@@ -239,7 +243,19 @@ const AppProvider = ({children}) => {
         }
     }
 
-    return <AppContext.Provider value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser, updateUser, handleChange, clearValues, createJob, getJobs, setEditJob, deleteJob, editJob}}>
+    const showStats = async() => {
+        dispatch({type: SHOW_STATS_BEGIN})
+        try {
+            const {data} = await authFetch('/jobs/stats')
+            dispatch({type: SHOW_STATS_SUCCESS, payload:{stats: data.defaultStats, monthlyApplications: data.monthlyApplications}})
+        } catch (error) {
+            console.log(error.response);
+            // logoutUser()
+        }
+        clearAlert()
+    }
+
+    return <AppContext.Provider value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser, updateUser, handleChange, clearValues, createJob, getJobs, setEditJob, deleteJob, editJob, showStats}}>
         {children}
     </AppContext.Provider>
 }
