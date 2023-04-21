46b1c4091845934fb036f4865e66c8754e6cb07f
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-21 13:24:32 +0530

    Edit, delete job- front end

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -15,7 +15,11 @@ import { DISPLAY_ALERT,
     CREATE_JOB_ERROR,
     GET_JOBS_BEGIN,
     GET_JOBS_SUCCESS,
-    SET_EDIT_JOB
+    SET_EDIT_JOB,
+    DELETE_JOB_BEGIN,
+    EDIT_JOB_BEGIN,
+    EDIT_JOB_SUCCESS,
+    EDIT_JOB_ERROR
     } from "./actions"
 
 import { initialState } from "./appContext"
@@ -183,6 +187,36 @@ const reducer = (state, action) => {
             jobLocation, 
             jobType,
             status
+        }
+    }
+    if(action.type === DELETE_JOB_BEGIN){
+        return{
+            ...state,
+            isLoading: true
+        }
+    }
+    if(action.type === EDIT_JOB_BEGIN){
+        return{
+            ...state,
+            isLoading: true
+        }
+    }
+    if(action.type === EDIT_JOB_SUCCESS){
+        return{
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'success',
+            alertText: 'Job Updated!'
+        }
+    }
+    if(action.type === EDIT_JOB_ERROR){
+        return{
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: action.payload.msg
         }
     }
 
